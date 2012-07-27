425473b8b91b9a211d4edb20d4b7ce8db16d5195
David Barnes <dave@advancedstyle.com>
2012-07-27 09:50:41 +0700

    Settings Added
    Added:
    - Options to pause when mouseover
    - Options to scroll right to left
    - Auto calcuate height
    - Adjusted HTML wrappers

--- js/jquery.simplemarquee.js
+++ js/jquery.simplemarquee.js
@@ -1,32 +1,59 @@
 jQuery(function($){
 	$.fn.simplemarquee = function(options){
 		var settings = $.extend( {
-		  'speed'         : 5000
+		  'speed'         : 5000,
+		  'direction' : 'rtl', // "rtl" (right to left) or "ltr" (left to right)
+		  'pause' : true,
+		  'wrapclass' : 'sections'
 		}, options);
 		var html = '';
 		var width = 0;
+		var animation;
 		return this.each(function() {
 			if($(this).length){
-				html = $(this).html()+'<span class="spacer"></span>';
-				$(this).html('<div class="section">'+html+'</div>');
-				width = $('.section',this).width();
+				html = '<div class="section">'+$(this).html()+'</div>';
+				$(this).html('<div class="'+settings.wrapclass+'">'+html+'</div>');
+				width = $('.'+settings.wrapclass,this).width();
+				if($(this).css('height') == '0px'){
+					$(this).height($('.section:eq(0)',this).height());
+				}
+				
 				var n = 0;
 				for(i=0;i<30;i++){
-					$('.section',this).append(html);
-					if($('.section',this).width() > $(this).width()){
+					$('.'+settings.wrapclass,this).append(html);
+					if($('.'+settings.wrapclass,this).width() > $(this).width()){
 						n++;
 					}
 					if(n > 1){
 						break;
 					}
 				}
-				animateit($('.section',this));
+				$('.'+settings.wrapclass,this).width('9999px');
+				animateit($('.'+settings.wrapclass,this),true);
+			}
+			if(settings.pause){
+				$('.'+settings.wrapclass,this).mouseover(function(){
+					animation.stop();
+				});
+				$('.'+settings.wrapclass,this).mouseout(function(){
+					animateit($(this),false);
+				});
 			}
 		});
-		function animateit(obj){
-			obj.animate({left: '-'+width+'px'},settings.speed,'linear',function(){
-				obj.css('left','0px');
-				animateit(obj);
+		function animateit(obj,rst){
+			if(settings.direction == 'ltr'){
+				if(rst){
+					obj.css('left','-'+(width*2)+'px');
+				}
+				var goto = (width-2);
+			}else{
+				if(rst){
+					obj.css('left','0px');
+				}
+				var goto = (width+2);
+			}
+			animation = obj.animate({left: '-'+goto+'px'},settings.speed,'linear',function(){
+				animateit(obj,true);
 			});
 		}
 	}
